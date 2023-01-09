d985e622cb1a5e7c30ef75d967b3d775be37762d
csandoval18 <christian_alvites@outlook.com>
2023-01-09 15:37:05 -0600

    update

--- client/components/Navbar/Navbar.tsx
+++ client/components/Navbar/Navbar.tsx
@@ -1,14 +1,22 @@
 import React from "react"
+import Image from "next/image"
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
 	return (
-		<nav className='flex justify-between align-middle px-60 backdrop-blur-sm bg-white/30 py-4 ='>
-			<div className='logo-wrapper'>
-				<img src='' alt='logo' />
+		<nav className='flex justify-between align-middle px-60 backdrop-blur-mm bg-gray-900 bg-opacity-90 py-4 ='>
+			<div className='bg-wrapper'>
+				<Image
+					className='invert'
+					src='/next.svg'
+					alt='Next.js Logo'
+					width={180}
+					height={37}
+					priority
+				/>
 			</div>
-			<ul className='nav-menu'>
+			<ul className='nav-menu flex gap-2 text-lg'>
 				<li>
 					<a href=''>Articles</a>
 				</li>
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,22 +2,14 @@ import React from "react"
 import Navbar from "../components/Navbar/Navbar"
 import styles from "../styles/Home.module.css"
 import Image from "next/image"
+import logo from "../public/next.svg"
 
 interface indexProps {}
 
 const index: React.FC<indexProps> = () => {
 	return (
 		<>
-			<Navbar>
-				<Image
-					className={styles.logo}
-					src='/next.svg'
-					alt='Next.js Logo'
-					width={180}
-					height={37}
-					priority
-				/>
-			</Navbar>
+			<Navbar />
 		</>
 	)
 }
