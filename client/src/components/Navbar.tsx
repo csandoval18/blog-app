3b5018e3383e021c87bec8bd171c8e66b3f89887
csandoval18 <christiansandoval1018@outlook.com>
2023-03-03 17:37:40 -0600

    creating hamburger button and adding transition animations

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,37 +1,50 @@
-import React from "react"
+import React, { useState } from "react"
 import Image from "next/image"
 import styles from "@/styles/components/Navbar.module.scss"
+import Link from "next/link"
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
+	const [openMenu, setOpenMenu] = useState(false)
 	return (
 		<nav className={styles.navbar}>
-			<Image
-				className={styles.logo}
-				src='/next.svg'
-				alt='Next.js Logo'
-				width={180}
-				height={37}
-				priority
-			/>
+			<Link href='/'>
+				<Image
+					className={styles.logo}
+					src='/next.svg'
+					alt='Next.js Logo'
+					width={180}
+					height={37}
+					priority
+				/>
+			</Link>
 			<ul className={styles.navMenu}>
 				<li>
-					<a href=''></a>
+					<a href='/services'>Services</a>
 				</li>
 				<li>
-					<a href=''></a>
+					<a href='/about'>About</a>
 				</li>
 				<li>
-					<a href=''></a>
+					<a href='/testimonials'>Testimonials</a>
 				</li>
 				<li>
-					<a href=''></a>
-				</li>
-				<li>
-					<a href=''>Contact</a>
+					<a href='/contact'>Contact</a>
 				</li>
 			</ul>
+			<div
+				className={
+					openMenu
+						? styles.hamburgerWrapper + " " + styles.open
+						: styles.hamburgerWrapper
+				}
+				onClick={() => {
+					setOpenMenu(!openMenu)
+				}}
+			>
+				<div className={styles.hamburger}></div>
+			</div>
 		</nav>
 	)
 }
